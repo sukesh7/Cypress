cc4a02d67f65ebdae5ce48a13ed65c3a75957ea4
antosukesh <antoadai@AntoSukesh.local>
2018-12-20 11:10:44 +0530

    [Anto] Updated framework

--- src/pageObjects/base_page.js
+++ src/pageObjects/base_page.js
@@ -1,29 +1,7 @@
 class base_page{
 
     constructor(){
-        this.txtUsername="#username";
-        this.txtPassword="#password";
-        this.btnSubmit="#kc-login";
-        this.hdrUserAccounts=":nth-child(2) > #header-menu";
-        this.hdrLnkManageUserAccounts=".open > .dropdown-menu > :nth-child(2) > a";
-        this.lblUserCreation="div[role='alert'] h5";
-    }
-
-    login(){
-        cy.visit("/");
-        cy.get(this.txtUsername).type(Cypress.env('username'));
-        cy.get(this.txtPassword).type(Cypress.env('password'));
-        cy.get(this.btnSubmit).click();
-    }
-
-    navigateToManageUserAccountsAction(button){
-        cy.get(this.hdrUserAccounts).click();
-        cy.get(this.hdrLnkManageUserAccounts).click();
-        cy.get("button").contains(button).click();
-    }
-
-    clickButton(action){
-        cy.get("button").contains(action).click();
+       
     }
 
 }
--- src/pageObjects/sample_page.js
+++ src/pageObjects/sample_page.js
@@ -1,42 +1,10 @@
-class create_user_page{
+class sample_page{
 
     constructor(){
-        this.hdrUserAccounts=":nth-child(2) > #header-menu";
-        this.hdrLnkCreateUserAccounts=".open > .dropdown-menu > :nth-child(1) > a";
-        this.txtUsername="#username";
-        this.txtDocumentType="#documentType";
-        this.txtPassword="#password";
-        this.txtAAAUserId="#aaaUserID";
-        this.txtT24UserId="#t24UserID";
-        this.ddlCountry="select#country";
-        this.ddlChannel="select#channel";
-        this.btnCreateUser="button.btn-success";
-        this.lblUserCreation="div[role='alert'] h5";
+        this.search="[name='q']";
+        this.searchButton="[name='btnG']";
     }
 
-    navigateToCreateUserPage(){
-        cy.get(this.hdrUserAccounts).click();
-        cy.get(this.hdrLnkCreateUserAccounts).click();
-    }
-
-    fillNewUserDetailsAndSubmit(username,documentType,password,aaaUserId,t24UserId,country){
-        cy.get(this.txtUsername).type(username);
-        cy.get(this.txtDocumentType).type(documentType);
-        cy.get(this.txtPassword).type(password);
-        cy.get(this.txtAAAUserId).type(aaaUserId);
-        cy.get(this.txtT24UserId).type(t24UserId);
-        cy.get(this.ddlCountry).select(country);
-        cy.get(this.btnCreateUser).click();
-    }
-
-    fillNewUserDetailsServiceDocAndSubmit(username,documentType,password,country,channel){
-        cy.get(this.txtUsername).type(username);
-        cy.get(this.txtDocumentType).type(documentType);
-        cy.get(this.txtPassword).type(password);
-        cy.get(this.ddlCountry).select(country);
-        cy.get(this.ddlChannel).select(channel);
-        cy.get(this.btnCreateUser).click();
-    }
 }
 
-module.exports = new create_user_page();
+module.exports = new sample_page();
